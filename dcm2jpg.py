094edf76f4478f62246db75e7867d1625ec8dbfb
eddiekragt <eddie.kragt@gmail.com>
2025-04-09 13:52:52 +0200

    Update dcm2jpg.py

--- dcm2jpg.py
+++ dcm2jpg.py
@@ -15,6 +15,10 @@ def dcm_to_jpg(dcm_path, output_dir):
         # Lees het DICOM bestand met SimpleITK
         dicom_image = sitk.ReadImage(dcm_path)
 
+        # Haal de SOP Instance UID op uit de metadata
+        sop_instance_uid = dicom_image.GetMetaData("0008|0018")
+        print(f"SOP Instance UID: {sop_instance_uid}")
+
         # Haal de 3D pixeldata op als een numpy array
         pixel_data = sitk.GetArrayFromImage(dicom_image)
         print(f"Pixeldata van het DICOM bestand: {pixel_data.shape}")
@@ -35,8 +39,8 @@ def dcm_to_jpg(dcm_path, output_dir):
             if image.mode != 'RGB':
                 image = image.convert('RGB')
 
-            # Sla de afbeelding op als JPG
-            output_path = os.path.join(output_dir, f"slice_{i+1}.jpg")
+            # Sla de afbeelding op als JPG met de SOP Instance UID en slice nummer
+            output_path = os.path.join(output_dir, f"{sop_instance_uid}_slice_{i+1}.jpg")
             image.save(output_path, 'JPEG')
             print(f"Slice {i+1} opgeslagen als {output_path}")
 
