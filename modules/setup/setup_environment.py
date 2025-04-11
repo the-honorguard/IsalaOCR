ac78420abdfbf14921e059eed484614dc0842771
thehonorguard <julianbeukinga@gmail.com>
2025-04-11 09:31:58 +0200

    .

--- modules/setup/setup_environment.py
+++ modules/setup/setup_environment.py
@@ -3,23 +3,30 @@ import sys
 import os
 
 def setup_virtualenv():
-    venv_path = "venv"
+    # Dynamisch het pad naar de bovenliggende map bepalen
+    parent_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    venv_path = os.path.join(parent_path, "venv")  # Virtuele omgeving in de map boven IsalaOCR
     if not os.path.exists(venv_path):
         print(f"Het aanmaken van de virtuele omgeving: {venv_path}")
         subprocess.run([sys.executable, "-m", "venv", venv_path])
 
 def install_requirements():
+    # Dynamisch het pad naar de bovenliggende map bepalen
+    parent_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    venv_path = os.path.join(parent_path, "venv")
+    pip_path = os.path.join(venv_path, "Scripts" if os.name == "nt" else "bin", "pip")
+    requirements_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "requirements.txt")  # Pad naar requirements.txt in modules/setup/
     print("Installeren van vereiste packages...")
-    subprocess.run([os.path.join("venv", "bin", "pip"), "install", "--upgrade", "pip"])
-    subprocess.run([os.path.join("venv", "bin", "pip"), "install", "-r", "requirements.txt"])
+    subprocess.run([pip_path, "install", "--upgrade", "pip"])
+    if os.path.exists(requirements_path):
+        subprocess.run([pip_path, "install", "-r", requirements_path])
+    else:
+        print(f"ERROR: Requirements-bestand niet gevonden: {requirements_path}")
 
-def run_ocr_script():
-    print("Runocr.py wordt uitgevoerd...")
-    subprocess.run([os.path.join("venv", "bin", "python"), "runocr.py"])
 
 if __name__ == "__main__":
     # Setup de virtuele omgeving
     setup_virtualenv()
 
     # Installeer vereisten
-    install_requirements()  
+    install_requirements()
